2cf6d2091587c835acc26cda45a683bd24665f05
teuzinYTBR <matheus.diegues@outlook.com>
2021-12-18 18:16:30 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event){
     if (event.keyCode === 32){
@@ -12,7 +13,7 @@ function handleKeyUp(event){
     }
 }
 function jump(){
-    let position = 0;
+    
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -39,10 +40,27 @@ function jump(){
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
-    
+    let randomTime = Math.random() * 6000;
+    console.log(randomTime);
+
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(()=>{
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //
+            clearInterval(leftInterval);
+            document.body.innerHTML = "<h1 class='game-over'>Fim de Jogo</h1/>"
+        }else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';    
+        }
+    }, 20);
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
