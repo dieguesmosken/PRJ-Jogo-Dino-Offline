a4cb24a6d1344eb46952df9b1d9f97f28f5e601d
teuzinYTBR <matheus.diegues@outlook.com>
2021-12-18 14:52:36 -0300

    remove spaces between rows

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 const dino = document.querySelector('.dino');
+
 let isJumping = false;
 //console.log(dino);
+
 function handleKeyUp(event){
     if (event.keyCode === 32){
         console.log('pressionou espaço!');
         if(!isJumping){
             jump();   
         }
-        
     }
 }
 function jump(){
     let position = 0;
-
     isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 200){
             clearInterval(upInterval);
-            //descendo
+
             let downInterval = setInterval(()=>{
                 if (position <= 0){
                     clearInterval(downInterval);
                     isJumping = false;
                 }else{
-                position -= 20;
-        
+                position -= 20;//descendo
                 dino.style.bottom = position + 'px';    
                 }
-                
             }, 20);
-            //subindo
-        }else{
+        }else{//subindo
         position += 20;
         
         dino.style.bottom = position + 'px';    
